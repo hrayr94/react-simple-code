5ef37aa2d1ca74526b107d569ec83f249f971b8f
Hrayr94 <hrayrashchyan94@gmail.com>
2023-04-22 11:25:09 +0400

    First commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+// import ReactDOM from "react-dom";
+import * as ReactDOMClient from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const inputClick = () => console.log("Clicked");
+const mouseOver = () => console.log("Mouse Over");
+
+const helpText = "Help text";
+
+const elements = (
+  <div className="name">
+    <h1>{helpText}</h1>
+    <input
+      placeholder={helpText}
+      onClick={inputClick}
+      onMouseEnter={mouseOver}
+    />
+    <p>{helpText === "Help text" ? "Yes" : "No"}</p>
+  </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const app = ReactDOMClient.createRoot(document.getElementById("app"));
+app.render(elements);
